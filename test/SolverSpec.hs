280d00aed9713f27cbc06b3ea5ab99a58c5239c3
Pontus Ernstedt <pontus_ponta@hotmail.com>
2021-01-11 10:06:52 +0100

    Add test

--- test/SolverSpec.hs
+++ test/SolverSpec.hs
@@ -9,9 +9,20 @@ import Solver
 import Checker
 import Data.Maybe (isJust, fromJust)
 
+
 spec :: Spec
 spec = do
   describe "solve" $ do
     it "produces only witnesses that passes the checker" $
       forAll (fmap solve genSmallBoard `suchThat` isJust)
-        (\b -> (check $ fromJust $ b) `shouldBe` Correct)
+        (\b -> (check $ fromJust b) `shouldBe` Correct)
+
+    it "does not remove any marks initially on the board" $
+      forAll genSmallBoard (\b -> 
+        let sameMarks :: Board -> Board -> Bool
+            sameMarks b b' = 
+              all (\(m, m') -> not (isMark m) || m == m') $ 
+                  zip (concat b) (concat b')
+            b' :: Maybe Board
+            b' = solve b 
+         in isJust b' ==> sameMarks b (fromJust b'))
